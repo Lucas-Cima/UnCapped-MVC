b48022b1bd8a3968b97a1e2599bab38c1da6043a
Lucas Cima <lucas.cima92@gmail.com>
2018-07-01 14:50:48 -0500

    Started Capstone Project

---
+++ src/main/java/com/uncapped/uncappedmvc/controllers/BeerController.java
@@ -0,0 +1,47 @@
+package com.uncapped.uncappedmvc.controllers;
+
+
+import com.uncapped.uncappedmvc.models.Beer;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("beer")
+public class BeerController {
+
+
+    @RequestMapping(value= "")
+    public String index(Model model) {
+
+        model.addAttribute("beers", beerDao.findAll());
+        model.addAttribute("title", "My List of Beers");
+
+        return "beer/index";
+    }
+
+    @RequestMapping(value = "check-in", method = RequestMethod.GET)
+    public String displayCheckInForm(Model model) {
+        model.addAttribute("title", "Check in New Beer");
+        model.addAttribute(new Beer());
+        return "beer/check-in";
+    }
+
+    @RequestMapping(value = "check-in", method = RequestMethod.POST)
+    public String processCheckInForm(@ModelAttribute @Valid Beer newBeer,
+                                         Errors errors, Model model) {
+
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Check in New Beer");
+            return "beer/check-in";
+        }
+        beerDao.save(newBeer);
+        return "redirect";
+    }
+
+}
